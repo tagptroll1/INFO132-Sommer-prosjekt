dc51b76e863ec680ffdd395a72a2db3bcfc3e1f6
Joachim Morken <joknumo@gmail.com>
2019-06-12 18:55:52 +0200

    Working example of multiple choice (hardcoded for now)

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,11 +1,53 @@
 <script>
-	export let name;
+	export let question = "Hva printer denne kodesnutten? \n tall = [0, 2, 3, 8, 9]\n for i in tall:\n \tif i % 2 == 0:\n\t\tprint(i, end=” “)"
+	export let answer = "b. 0 2 8"
+	export let choices = ["a. 3 9", "b. 0 2 8", "c. ingenting", "d. 2 8"]	
+
+	
+	function clickHandler(event) {
+		if (event.toElement.innerText == answer) {
+			console.log("correct")
+		}
+	}
+
+
 </script>
 
 <style>
 	h1 {
-		color: purple;
+		color: aquamarine;
 	}
+
+	#question {
+		white-space: pre-wrap;
+	}
+
+	button {
+		font-size: 30px;
+	}
+
+
+
+	
+
+
+
 </style>
 
-<h1>Hello {name}!</h1>
+
+
+<div id ="multiple_choice"> 
+	<h1>Multiple Choice</h1>
+	
+	<h2 id="question">{question}</h2>
+
+	<ul>
+		{#each choices as choice} 
+			<li id="choice">
+				<button on:click={clickHandler}>{choice}</button>
+			</li>
+		{/each}
+	</ul>
+
+
+</div>
--- client/src/main.js
+++ client/src/main.js
@@ -3,7 +3,8 @@ import App from './App.svelte';
 const app = new App({
 	target: document.body,
 	props: {
-		name: 'world'
+		question: "Hva printer denne kodesnutten? \n tall = [0, 2, 3, 8, 9]\n for i in tall:\n \tif i % 2 == 0:\n\t\tprint(i, end=” “)"
+		
 	}
 });
 
