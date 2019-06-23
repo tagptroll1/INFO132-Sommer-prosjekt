ea6c18ceca39f24bf31b32904699e151513eb51b
Thomas Petersson <thomas@petersson.priv.no>
2019-06-23 17:55:10 +0200

    feat(converted to sapper + Codeblock component)

---
+++ app/src/components/Codeblock.svelte
@@ -0,0 +1,18 @@
+<script>
+    import hljs from 'highlight.js';
+
+    export let code;
+    let stuff = hljs.highlight("python", code);
+
+</script>
+
+
+<style>
+    code {
+        display: block;
+        padding: 5px;
+        border-radius: 5px;
+    }
+</style>
+
+<pre><code class="python">{@html stuff.value}</code></pre>
