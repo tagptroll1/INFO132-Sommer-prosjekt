db55c2e466fdd8306a7794185554265640a69a11
Joachim Morken <joknumo@gmail.com>
2019-08-28 14:53:22 +0200

    Dropdown component

---
+++ app/src/routes/question/_components/_Drop.svelte
@@ -0,0 +1,35 @@
+
+
+<script>
+  import { slide } from "svelte/transition";
+
+  let isOpen = false;
+  let selected = "";
+
+  import questions from "../../../stores/questions";
+  import question from "../../../stores/question";
+  import index from "../../../stores/index";
+  import user from "../../../stores/user";
+
+let showAlternatives = false;
+function handleSelect(event){
+  selected = event.target.getAttribute("data-value");
+  //event.target.style.background = "grey"
+}
+</script>
+
+<style>
+
+</style>
+
+<section>
+<h1 on:click={()=>showAlternatives = !showAlternatives}>SE ALTERNATIVER</h1>
+{#if showAlternatives}
+  <div transition:slide>
+    {#each $question.alternatives as alternative}
+      <button id={alternative} on:click={handleSelect} data-value={alternative} style="">{alternative}</button><br>
+    {/each}
+  </div>
+{/if}
+</section>
+
