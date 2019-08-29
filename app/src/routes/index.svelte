b460e444588cb760b7032678dc43486f2124fcd3
practicalRope <hkmelaa@hotmail.com>
2019-08-29 22:04:20 +0200

    better

--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -1,20 +1,55 @@
 <script>
   import { goto } from "@sapper/app"
-  import Logo from "../components/LogoUIB.svelte";
+  import { onMount } from 'svelte';
 
   import user from "../stores/user.js";
   import index from "../stores/index";
   import questions from "../stores/questions";
 
+  import Logo from "../components/LogoUIB.svelte";
+
   $index = 0;
   $questions = [];
+
+  let canvas;
+  let ctx;
+  let color;
+  let mx,my;
+  onMount(()=>{
+    ctx = canvas.getContext('2d')
+
+    // color = getComputedStyle(document.documentElement)
+    //         .getPropertyValue('--bg-aside');
+
+    function loop(t) {
+			requestAnimationFrame(loop);
+
+			const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+			for (let p = 0; p < imageData.data.length; p += 4) {
+				const i = p / 4;
+				const x = i % canvas.width;
+				const y = i / canvas.height >>> 0;
+
+				const r = 64 + (128 * x / canvas.width) + (64 * Math.sin(t / 2500));
+				const g = 64 + (128 * y / canvas.height) + (64 * Math.cos(t / 2500));
+				const b = 128;
+
+				imageData.data[p + 0] = r * .9;
+				imageData.data[p + 1] = g * .4;
+				imageData.data[p + 2] = b * .7;
+				imageData.data[p + 3] = 255;
+			}
+
+			ctx.putImageData(imageData, 0, 0);
+    } requestAnimationFrame(loop);
+  });
 
   const pattern = `[a-zA-ZæøåÆØÅ]{3}\\d{3}`;
 
   let input;
   let value = '';
   let placeholder = "ex; rob404";
-
   let numberOfAttempts = 0;
 
   const alternatives = [
@@ -56,6 +91,10 @@
     numberOfAttempts++;
   }
 
+  function mousemove(event){
+    mx = event.clientX;
+    my = event.clientY;
+  }
 </script>
 <style>
   :global(.err){
@@ -71,12 +110,12 @@
   }
 
   .outer{
-    color: var(--color-main);
+    color: var(--bg-main);
     display: flex;
     flex-direction: column;
     align-items: center;
 
-    background-color: hsla(0, 0%, 77%, 0.6);
+    /* background-color: hsla(0, 0%, 77%, 0.6); */
     padding: 20px 30px;
   }
 
@@ -84,7 +123,7 @@
     text-align: center;
     font-weight: 300;
     font-size: 3.2em;
-    margin-top: 30px;
+    margin: 10px 0;
   }
 
   .start {
@@ -140,14 +179,16 @@
     box-shadow: none;
   }
 
-  figure {
+  canvas{
     position: fixed;
     z-index: -1;
     display: flex;
     justify-content: center;
     align-items: center;
     overflow: hidden;
-    width: 100vw;
+
+    width: 100%;
+		height: 100%;
   }
 
   .inpblock{
@@ -157,15 +198,34 @@
 
   .inpblock span{
     text-align: center;
-    margin: 5px;
+    margin: 10px;
   }
 
+  
+  figure{
+    max-width: 128px;
+    max-height: 128px;
+  }
+
+  .inpblock input,.start{
+    box-shadow: 1px 3px 1px -2px rgba(0, 0, 0, 0.3);
+  }
+  h1,span{
+    text-shadow: 1px 1px 3px rgba(0, 0, 0, 0.8);
+  }
+
+
 </style>
-<figure>
-  <Logo />
-</figure>
+<svelte:window on:mousemove={mousemove}/>
+<canvas
+	bind:this={canvas}
+	width={10}
+	height={10}
+></canvas>
+
 
 <div class="outer">
+  <figure><Logo/></figure>
   <h1>UiB Python</h1>
 
   <div class="inpblock">
